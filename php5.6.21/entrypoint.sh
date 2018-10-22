068d29a5fa3f5f430e54f8d1d520832d18d16343
Lars Feldeisen <lars@feldeisen.de>
2018-10-22 13:33:43 +0200

    Update entrypoint.sh

--- php5.6.21/entrypoint.sh
+++ php5.6.21/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-##build certs
-[ -f /etc/apache2/ssl/server.key ] || openssl genrsa -out /etc/apache2/ssl/server.key 2048
-[ -f /etc/apache2/ssl/server.csr ] || openssl req -new -key /etc/apache2/ssl/server.key -out /etc/apache2/ssl/server.csr -subj '/CN=/C=DE'
-[ -f /etc/apache2/ssl/server.crt ] || openssl x509 -req -days 3650 -in /etc/apache2/ssl/server.csr -signkey /etc/apache2/ssl/server.key -out /etc/apache2/ssl/server.crt
-##############
-
 cd /var/www/html && echo "curl $SOURCE_DOWNLOAD_LINK > download_source" >> download.sh && chmod +x download.sh && ./download.sh && rm download.sh
 unzip -o -q download_source && rm download_source
 
